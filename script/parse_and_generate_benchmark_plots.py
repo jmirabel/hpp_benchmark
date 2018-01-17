d867354c5f5cab535d06099af79a78761bd4ba53
Joseph Mirabel <jmirabel@laas.fr>
2018-01-17 14:39:02 +0100

    Add scripts to generate plots of results

---
+++ script/parse_and_generate_benchmark_plots.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import os, sys
+from parse_benchmark import parseDirectories, generateCSV
+from generate_benchmark_plot import readCSV, generatePlot
+
+# To generate this list:
+# find 20*/* -type d | sed 's/^[^\/]*\///' | sort | uniq
+
+if len(sys.argv) > 1:
+    benchmarks = sys.argv[1:]
+else:
+    benchmarks=[
+            "baxter-manipulation-boxes",
+            "baxter-manipulation-boxes-easy",
+            "baxter-two-arms-one-box",
+            "baxter-two-arms-three-boxes",
+            "baxter-two-arms-two-boxes",
+            "hrp2-on-the-ground",
+            "kawada",
+            "path_validation_manipulation",
+            "pr2-in-iai-kitchen",
+            "pr2-ini-iai-kitchen",
+            "pr2-manipulation-kitchen",
+            "pr2-manipulation-two-hand",
+            "ur5_path_projection",
+            "ur5-spline-optimization",
+            ]
+
+root_dir="."
+output_dir="./results"
+script_dir="./script"
+
+if not os.path.isdir(output_dir): os.mkdir(output_dir)
+
+for bench in benchmarks:
+    csv = os.path.join (output_dir, bench + ".csv")
+
+    keys, values = parseDirectories (root_dir, bench)
+
+    if len(keys) == 0:
+        print("No data for bench " + bench)
+        continue
+
+    with open(csv, 'w') as csvfile:
+        generateCSV (keys, values, csvfile)
+
+    legends, labels, rows = readCSV(csv)
+    if len(rows) == 0:
+        print("This should not happen: No data in file " + csv)
+        continue
+    svg = os.path.join (output_dir, bench + ".svg")
+    generatePlot (legends, labels, rows, bench, svg)
---
+++ script/parse_benchmark.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+from os.path import isdir, join, isfile
+
+def parseDirectories(directory, bench):
+    def valid (dir):
+        base = join(directory,dir)
+        if not isdir(base): return False
+        bdir = join(base,bench)
+        if not isdir(bdir): return False
+        return isfile(join(bdir,"benchmark"))
+
+    from os import listdir
+    keys = []
+    values = dict()
+
+    for dir in filter (valid, listdir(directory)):
+        file = join (directory,dir,bench,"benchmark")
+        if not isfile(file):
+            print(file + " does not exist", file=sys.stderr)
+            continue
+        values[dir] = dict()
+        with open (file, 'r') as f:
+            for line in f.readlines():
+                if line.startswith("Average "):
+                    k, v = line[len("Average "):].split(':',1)
+                    if k not in keys: keys.append(k)
+                    values[dir][k] = v
+    return keys, values
+
+def generateCSV(keys, values, csvfile, sep = ';', nan = 'nan'):
+    labels = sorted(values.keys())
+    print("Dates" + sep + sep.join(keys), file=csvfile)
+    for d in labels:
+        line = d
+        val = values[d]
+        # print(val)
+        for k in keys:
+            if val.has_key(k):
+                line += sep + str(float(val[k]))
+            else:
+                line += sep + nan
+        print(line, file=csvfile)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        raise RuntimeError("Expected 1 argument")
+
+    directory = sys.argv[1]
+    bench = sys.argv[2]
+
+    keys, values = parseDirectories (directory, bench)
+    generateCSV (keys, values, sys.stdout)
