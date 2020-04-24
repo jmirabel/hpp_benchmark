135cfa7427431abb84c15b1213a91fd9a361678c
Joseph Mirabel <jmirabel@laas.fr>
2020-04-24 11:58:03 +0200

    Make C++ benchmark produce CSV output files.

--- src/benchmark.cc
+++ src/benchmark.cc
@@ -1,7 +1,14 @@
 #include "benchmark.hh"
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <cstring>
+#include <chrono>
+#include <ctime>
 #include <iostream>
+#include <iomanip>
 #include <math.h>
 #include <hpp/util/timer.hh>
 
@@ -20,6 +27,140 @@ void print_header(std::ostream& os, const std::string& status)
   for (int i = 0; i < after ; ++i) os << ' ';
   os << ']' << ' ';
 }
+
+class NullStream : public std::ostream {
+  class NullBuffer : public std::streambuf {
+    public:
+      int overflow( int c ) { return c; }
+  } m_nb;
+  public:
+  NullStream() : std::ostream( &m_nb ) {}
+};
+
+bool dirExists(const char* d)
+{
+  struct stat info;
+
+  if(stat(d, &info) != 0)
+    return false;
+  else if(!(info.st_mode & S_IFDIR))
+    return false;
+  return true;
+}
+
+struct Output {
+  Output ()
+    : out_ (&std::cout)
+    , res_ (&nullStream_)
+  {
+    using std::chrono::system_clock;
+    system_clock::time_point now = system_clock::now();
+    std::time_t t_now = system_clock::to_time_t(now);
+    std::ostringstream oss;
+    oss << std::put_time(std::localtime(&t_now), "%F");
+    today = oss.str();
+  }
+
+  void print_header(const char* status)
+  {
+    ::print_header(o(), status);
+  }
+
+  void init_progress_bar (int N)
+  {
+    for (int i = 0; i < N; ++i) o() << '.';
+    for (int i = 0; i < N; ++i) o() << '\b';
+    o() << std::flush;
+  }
+
+  void pb_ok ()
+  {
+    o() << '|' << std::flush;
+  }
+
+  void pb_bad ()
+  {
+    o() << '-' << std::flush;
+  }
+
+  void setOutputDir(const char *dir)
+  {
+    if (!dirExists(dir)) {
+      std::cerr << "Directory does not exists: " << dir << std::endl;
+      return;
+    }
+    outputDir_ = dir;
+  }
+
+  bool setupResultFileFromBenchmarkName(const std::string& benchName)
+  {
+    if (outputDir_.empty()) return false;
+    std::string filename = outputDir_ + '/' + benchName;
+    if (resFile_.is_open()) resFile_.close();
+    resFile_.open(filename);
+    res_ = &resFile_;
+    return resFile_.good();
+  }
+
+  bool setupResultFileFromBenchmarkSubcaseName(const std::string& benchName,
+      const std::string& caseName)
+  {
+    if (outputDir_.empty()) return false;
+    std::string dir (outputDir_ + '/' + benchName);
+    if (!dirExists(dir.c_str())) {
+      if (mkdir(dir.c_str(), 0777) != 0) {
+        std::cerr << "Could not create directory " << dir << std::endl;
+        res_ = &nullStream_;
+        return false;
+      }
+    }
+    std::string filename = dir + '/' + caseName;
+    if (resFile_.is_open()) resFile_.close();
+    resFile_.open(filename);
+    res_ = &resFile_;
+    return resFile_.good();
+  }
+
+  void writeResult(const hpp::benchmark::results_t& results)
+  {
+    std::vector< std::vector<hpp::benchmark::value_type> > csvContent;
+    bool first = true;
+    r() << "Date";
+    for (auto& result : results) {
+      if (first) csvContent.resize(result.second.size());
+      r() << "; " << result.first;
+      int i = 0;
+      for (auto v : result.second) csvContent[i++].push_back(v);
+      first = false;
+    }
+    r() << '\n';
+    for (const auto& line : csvContent) {
+      r() << today;
+      for (auto v : line)
+        r() << "; " << v;
+      r() << '\n';
+    }
+    closeResultFile();
+  }
+
+  void closeResultFile()
+  {
+    resFile_.close();
+  }
+
+  std::ostream& o() { return *out_; };
+  std::ostream& r() { return *res_; };
+
+  std::string outputDir_;
+  std::string today;
+
+  std::ofstream outFile_;
+  std::ofstream resFile_;
+  NullStream nullStream_;
+
+  std::ostream* out_;
+  std::ostream* res_;
+} output;
 
 namespace hpp {
 namespace benchmark {
@@ -53,15 +194,13 @@ void BenchmarkCase::run (int N, const std::string& name)
 {
   debug::Timer timer;
 
-  print_header(std::cout, "RUN");
-  std::cout << "Benchmark " << name << ' ' << std::flush;
+  output.print_header("RUN");
+  output.o() << "Benchmark " << name << ' ' << std::flush;
 
   setup(N);
 
-  hpp::benchmark::results_t results;
-  for (int i = 0; i < N; ++i) std::cout << '.';
-  for (int i = 0; i < N; ++i) std::cout << '\b';
-  std::cout << std::flush;
+  results_t results;
+  output.init_progress_bar(N);
   for (int i = 0; i < N; ++i) {
     initializeProblem(i);
     timer.start();
@@ -69,20 +208,21 @@ void BenchmarkCase::run (int N, const std::string& name)
     timer.stop();
     results["Time (s)"].push_back(1e-3 * static_cast<value_type>(timer.duration().total_milliseconds()));
     saveResolutionResult(results);
-    if (validateSolution())
-      std::cout << '|' << std::flush;
-    else
-      std::cout << '-' << std::flush;
+    if (validateSolution()) output.pb_ok();
+    else                    output.pb_bad();
   }
 
   clean();
-  std::cout << std::endl;
+  output.o() << std::endl;
+
+  if (output.setupResultFileFromBenchmarkName(name))
+    output.writeResult(results);
 
   value_type mean, std_dev;
   for (auto& result : results) {
-    print_header(std::cout, "Result");
+    output.print_header("Result");
     stats(result.second, mean, std_dev);
-    std::cout << result.first << '\t' << mean << " +/- " << std_dev << '\n';
+    output.o() << result.first << '\t' << mean << " +/- " << std_dev << '\n';
   }
 }
 
@@ -90,21 +230,19 @@ void BenchmarkNCase::run (int N, const std::string& name)
 {
   debug::Timer timer;
 
-  print_header(std::cout, "SETUP");
-  std::cout << "Benchmark " << name << ' ' << std::flush;
+  output.print_header("SETUP");
+  output.o() << "Benchmark " << name << ' ' << std::flush;
   setup(N);
 
-  std::cout << '\n';
+  output.o() << '\n';
   std::vector<std::string> cases (names());
   int iCase = 0;
   for (const std::string& ncase : cases) {
-    print_header(std::cout, "RUN");
-    std::cout << "Benchmark " << name << '/' << ncase << ' ' << std::flush;
+    output.print_header("RUN");
+    output.o() << "Benchmark " << name << '/' << ncase << ' ' << std::flush;
 
-    hpp::benchmark::results_t results;
-    for (int i = 0; i < N; ++i) std::cout << '.';
-    for (int i = 0; i < N; ++i) std::cout << '\b';
-    std::cout << std::flush;
+    results_t results;
+    output.init_progress_bar(N);
     for (int i = 0; i < N; ++i) {
       initializeProblem(i, iCase);
       timer.start();
@@ -112,20 +250,20 @@ void BenchmarkNCase::run (int N, const std::string& name)
       timer.stop();
       results["Time (s)"].push_back(1e-3 * static_cast<value_type>(timer.duration().total_milliseconds()));
       saveResolutionResult(results);
-      if (validateSolution())
-        std::cout << '|' << std::flush;
-      else
-        std::cout << '-' << std::flush;
+      if (validateSolution()) output.pb_ok();
+      else                    output.pb_bad();
     }
-    std::cout << std::endl;
+    output.o() << std::endl;
 
+    if (output.setupResultFileFromBenchmarkSubcaseName(name, ncase))
+      output.writeResult(results);
     value_type mean, std_dev;
     for (auto& result : results) {
-      print_header(std::cout, "Result");
       stats(result.second, mean, std_dev);
-      std::cout << result.first << '\t' << mean << " +/- " << std_dev << '\n';
+      output.print_header("Result");
+      output.o() << result.first << '\t' << mean << " +/- " << std_dev << '\n';
     }
-    std::cout << std::flush;
+    output.o() << std::flush;
     ++iCase;
   }
 
@@ -150,13 +288,16 @@ public:
 void usage(const char* name)
 {
   std::cout << name
-    << " --help        show usage and exit\n"
-    << " --list        list benchmarks and exit\n"
-    << " --run <name>  run only one benchmark. All options after this are ignored.\n"
-    << " -N <integer>  set the number of repetition for a benchmark\n"
+    << " --help           show usage and exit\n"
+    << " --list           list benchmarks and exit\n"
+    << " --run <name>     run only one benchmark. All options after this are ignored.\n"
+    << " --output <dir>   directory where to write the benchmark results.\n"
+    << " --today <date>   date written in the result file indicating when the script was run.\n"
+    << " -N <integer>     set the number of repetition for a benchmark\n"
     << std::flush;
 }
 
+#define CHECK_REMAINING_ARGC(i,n,msg) if (i + n >= argc) { std::cerr << "Expected " << n << " argument(s) after " << msg << '\n'; return 1; }
 int main(int argc, char**argv)
 {
   using hpp::benchmark::BenchmarkRunner;
@@ -174,6 +315,7 @@ int main(int argc, char**argv)
         std::cout << pair.first << '\n';
       return 0;
     } else if (strcmp(argv[iarg], "--run") == 0) {
+      CHECK_REMAINING_ARGC(iarg, 1, "--run");
       std::string name (argv[++iarg]);
       if (problems.count(name) == 0) {
         std::cerr << name << " not found.\n";
@@ -181,7 +323,14 @@ int main(int argc, char**argv)
       }
       BenchmarkRunner::run(N, name, problems[name]);
       return 0;
+    } else if (strcmp(argv[iarg], "--output") == 0) {
+      CHECK_REMAINING_ARGC(iarg, 1, "--output");
+      output.setOutputDir(argv[++iarg]);
+    } else if (strcmp(argv[iarg], "--date") == 0) {
+      CHECK_REMAINING_ARGC(iarg, 1, "--date");
+      output.today = argv[++iarg];
     } else if (strcmp(argv[iarg], "-N") == 0) {
+      CHECK_REMAINING_ARGC(iarg, 1, "-N");
       N = (int)strtol(argv[++iarg],NULL,0);
     }
   }
